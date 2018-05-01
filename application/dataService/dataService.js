cd03dcd4c2aeacba0a186707806baac803620d7f
GuillaumeUVHC <croy.guillaume@outlook.fr>
2018-05-01 20:48:31 +0200

    recuperation de cases dans plateau OK

--- application/dataService/dataService.js
+++ application/dataService/dataService.js
@@ -2,19 +2,21 @@ export default class dataService {
 
     constructor($http) {
         this.$http = $http;
+        this.backUrl = "http://localhost:8081/RestServer/api/monopoly";
     }
 
     //get all the player
     getJoueurs() {
-        return this.$http.get("http://192.168.137.1:8080/RestServer/api/monopoly/joueurs");
+       // return this.$http.get("http://localhost:8081/RestServer/api/monopoly/joueurs");
+        return this.$http.get(this.backUrl+"/joueurs");
     }
 
     getCases() {
-        return this.$http.get("http://192.168.137.1:8080/RestServer/api/monopoly/cases")
+        return this.$http.get(this.backUrl+"/cases");
     }
 
     getCase(idCase) {
-        return this.$http.get(`http://192.168.137.1:8080/RestServer/api/monopoly/cases/${idCase}`)
+        return this.$http.get(this.backUrl+`/cases/${idCase}`);
     }
 
     //http://192.168.137.1:8080/RestServer/api/monopoly/cases/12
--- application/plateau/plateauComponent.js
+++ application/plateau/plateauComponent.js
@@ -1,12 +1,25 @@
 import plateauHTML from "./plateau.html";
 
 class PlateauComponentController {
-	constructor() {
+	constructor(dataService) {
 		this.parkGratuitBinding = "Parking Gratuit";
+		this.dataService = dataService;
 	}
+	
+	$onInit() {
+		this.dataService.getCases()
+		.then(data => {
+			console.log('Cases', data.data);
+			this.cases = data.data;
+		})
+		.catch(error => {
+            console.log('Erreur sur le tableau', error);
+        })
+	}
+	
 }
 
-PlateauComponentController.$inject = [];
+PlateauComponentController.$inject = ['dataService'];
 
 export const plateau = {
 		template: plateauHTML,
